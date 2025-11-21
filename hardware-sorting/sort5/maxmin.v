ba0a80d3b7d2a112775ac000ffa5c02ca49a15dd
Sriram <120776139+kvsriram11@users.noreply.github.com>
2025-11-20 18:57:30 -0800

    Add files via upload

---
+++ hardware-sorting/sort5/maxmin.v
@@ -0,0 +1,17 @@
+module maxmin (max,min,A,B);
+input [7:0] A,B;
+output [7:0] max,min;
+
+wire [7:0] min,max;
+wire gt;
+
+/*
+assign min = gt?B:A;
+assign max = gt?A:B;
+assign gt = (A>B);
+*/
+
+assign {max,min} = gt ? {A,B} : {B,A};
+assign gt = A>B;
+
+endmodule
---
+++ hardware-sorting/sort5/mmmmm.do
@@ -0,0 +1,12 @@
+vlib work
+vdel -all
+vlib work
+
+vlog maxmin.v
+vlog mmm.v
+vlog mmmm.v
+vlog mmmmm.v
+vlog mmmmm_tb.v
+
+vsim work.tb
+run -all
